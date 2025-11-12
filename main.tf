29a41be68b3f3858f057ee8efb9c4d069d533d9c
keerthana-ai123 <keerthanadevathu123@gmail.com>
2025-11-12 13:10:25 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,16 +1,31 @@
+# Add the random provider in your root module if not already present:
+# terraform {
+#   required_providers {
+#     random = {
+#       source  = "hashicorp/random"
+#       version = ">= 3.0.0"
+#     }
+#   }
+# }
+
+# Unique suffix for AMI names to avoid duplicate name errors
+resource "random_id" "ami_suffix" {
+  byte_length = 3
+}
+
 # Create EC2 instance
 resource "aws_instance" "main" {
-    ami = local.ami_id
-    instance_type = "t3.micro"
-    vpc_security_group_ids = [local.sg_id]
-    subnet_id = local.private_subnet_id
-    
-    tags = merge (
-        local.common_tags,
-        {
-            Name = "${local.common_name_suffix}-${var.component}" # roboshop-dev-mongodb
-        }
-    )
+  ami                    = local.ami_id
+  instance_type          = "t3.micro"
+  vpc_security_group_ids = [local.sg_id]
+  subnet_id              = local.private_subnet_id
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-${var.component}" # roboshop-dev-mongodb
+    }
+  )
 }
 
 
@@ -18,7 +33,7 @@ resource "terraform_data" "main" {
   triggers_replace = [
     aws_instance.main.id
   ]
-  
+
   connection {
     type     = "ssh"
     user     = "ec2-user"
@@ -27,14 +42,14 @@ resource "terraform_data" "main" {
   }
 
   provisioner "file" {
-    source = "bootstrap.sh"
+    source      = "bootstrap.sh"
     destination = "/tmp/bootstrap.sh"
   }
 
   provisioner "remote-exec" {
     inline = [
-        "chmod +x /tmp/bootstrap.sh",
-        "sudo sh /tmp/bootstrap.sh ${var.component} ${var.environment}"
+      "chmod +x /tmp/bootstrap.sh",
+      "sudo sh /tmp/bootstrap.sh ${var.component} ${var.environment}"
     ]
   }
 }
@@ -42,53 +57,52 @@ resource "terraform_data" "main" {
 resource "aws_ec2_instance_state" "main" {
   instance_id = aws_instance.main.id
   state       = "stopped"
-  depends_on = [terraform_data.main]
+  depends_on  = [terraform_data.main]
 }
 
 resource "aws_ami_from_instance" "main" {
-  name               = "${local.common_name_suffix}-${var.component}-ami"
+  # use random suffix to make AMI names unique and avoid InvalidAMIName.Duplicate
+  name               = "${local.common_name_suffix}-${var.component}-ami-${random_id.ami_suffix.hex}"
   source_instance_id = aws_instance.main.id
-  depends_on = [aws_ec2_instance_state.main]
-  tags = merge (
-        local.common_tags,
-        {
-            Name = "${local.common_name_suffix}-${var.component}-ami" # roboshop-dev-mongodb
-        }
+  depends_on         = [aws_ec2_instance_state.main]
+
+  tags = merge(
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-${var.component}-ami-${random_id.ami_suffix.hex}"
+    }
   )
 }
 
 resource "aws_lb_target_group" "main" {
-  name     = "${local.common_name_suffix}-${var.component}"
-  port     = local.tg_port # if frontend port is 80, otherwise port is 8080
-  protocol = "HTTP"
-  vpc_id   = local.vpc_id
-  deregistration_delay = 60 # waiting period before deleting the instance
+  name                  = "${local.common_name_suffix}-${var.component}"
+  port                  = local.tg_port
+  protocol              = "HTTP"
+  vpc_id                = local.vpc_id
+  deregistration_delay  = 60
 
   health_check {
-    healthy_threshold = 2
-    interval = 10
-    matcher = "200-299"
-    path = local.health_check_path
-    port = local.tg_port
-    protocol = "HTTP"
-    timeout = 2
+    healthy_threshold   = 2
+    interval            = 10
+    matcher             = "200-299"
+    path                = local.health_check_path
+    port                = local.tg_port
+    protocol            = "HTTP"
+    timeout             = 2
     unhealthy_threshold = 2
   }
 }
 
 resource "aws_launch_template" "main" {
-  name = "${local.common_name_suffix}-${var.component}"
-  image_id = aws_ami_from_instance.main.id
-
+  name                               = "${local.common_name_suffix}-${var.component}"
+  image_id                           = aws_ami_from_instance.main.id
   instance_initiated_shutdown_behavior = "terminate"
-  instance_type = "t3.micro"
+  instance_type                      = "t3.micro"
 
   vpc_security_group_ids = [local.sg_id]
 
-  # when we run terraform apply again, a new version will be created with new AMI ID
   update_default_version = true
 
-  # tags attached to the instance
   tag_specifications {
     resource_type = "instance"
 
@@ -100,7 +114,6 @@ resource "aws_launch_template" "main" {
     )
   }
 
-  # tags attached to the volume created by instance
   tag_specifications {
     resource_type = "volume"
 
@@ -112,14 +125,12 @@ resource "aws_launch_template" "main" {
     )
   }
 
-  # tags attached to the launch template
   tags = merge(
-      local.common_tags,
-      {
-        Name = "${local.common_name_suffix}-${var.component}"
-      }
+    local.common_tags,
+    {
+      Name = "${local.common_name_suffix}-${var.component}"
+    }
   )
-
 }
 
 resource "aws_autoscaling_group" "main" {
@@ -130,22 +141,24 @@ resource "aws_autoscaling_group" "main" {
   health_check_type         = "ELB"
   desired_capacity          = 1
   force_delete              = false
+
   launch_template {
     id      = aws_launch_template.main.id
     version = aws_launch_template.main.latest_version
   }
-  vpc_zone_identifier       = local.private_subnet_ids
-  target_group_arns = [aws_lb_target_group.main.arn]
+
+  vpc_zone_identifier = local.private_subnet_ids
+  target_group_arns   = [aws_lb_target_group.main.arn]
 
   instance_refresh {
     strategy = "Rolling"
     preferences {
-      min_healthy_percentage = 50 # atleast 50% of the instances should be up and running
+      min_healthy_percentage = 50
     }
     triggers = ["launch_template"]
   }
-  
-  dynamic "tag" {  # we will get the iterator with name as tag
+
+  dynamic "tag" {
     for_each = merge(
       local.common_tags,
       {
@@ -162,7 +175,6 @@ resource "aws_autoscaling_group" "main" {
   timeouts {
     delete = "15m"
   }
-
 }
 
 resource "aws_autoscaling_policy" "main" {
@@ -199,8 +211,9 @@ resource "terraform_data" "main_local" {
   triggers_replace = [
     aws_instance.main.id
   ]
-  
+
   depends_on = [aws_autoscaling_policy.main]
+
   provisioner "local-exec" {
     command = "aws ec2 terminate-instances --instance-ids ${aws_instance.main.id}"
   }
